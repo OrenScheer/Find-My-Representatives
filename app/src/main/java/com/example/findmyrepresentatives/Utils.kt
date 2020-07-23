9fe9f8ba91a70aa1aa285e4e524734d57de1a2ba
Oren Scheer <oren.rs723@gmail.com>
2020-07-22 23:34:38 -0400

    Add Utils class for auxiliary methods

---
+++ app/src/main/java/com/example/findmyrepresentatives/Utils.kt
@@ -0,0 +1,9 @@
+package com.example.findmyrepresentatives
+
+class Utils {
+    companion object {
+        fun isValidPostalCode(postalCode: String): Boolean {
+            return false
+        }
+    }
+}
