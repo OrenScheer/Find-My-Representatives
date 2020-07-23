5db9d4651b12f88da7c6767552a036ae778a887e
Oren Scheer <oren.rs723@gmail.com>
2020-07-22 23:31:28 -0400

    Call function when search button is pressed

--- app/src/main/java/com/example/findmyrepresentatives/MainActivity.kt
+++ app/src/main/java/com/example/findmyrepresentatives/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.findmyrepresentatives
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun enterPostalCode(view: View) {
+        val searchBox = findViewById<TextView>(R.id.searchBox)
+        val postalCode = searchBox.text.toString()
     }
 }
