328c922011e65186c0fa7c30d9df862cfcc1f528
Oren Scheer <oren.rs723@gmail.com>
2020-07-25 22:54:34 -0400

    Add new data type for a representative

---
+++ app/src/main/java/com/example/findmyrepresentatives/Representative.kt
@@ -0,0 +1,11 @@
+package com.example.findmyrepresentatives
+
+class Representative (
+    val photo_url: String,
+    val first_name: String,
+    val last_name: String,
+    val district_name: String,
+    val representative_set_name: String,
+    val elected_office: String,
+    val email: String,
+)
