9a97160f52f20021dc551c57a8686939df54e20d
Oren Scheer <oren.rs723@gmail.com>
2020-07-30 00:39:14 -0400

    Update KDoc

--- app/src/main/java/com/example/findmyrepresentatives/RecyclerAdapter.kt
+++ app/src/main/java/com/example/findmyrepresentatives/RecyclerAdapter.kt
@@ -12,7 +12,7 @@ import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 import java.util.*
 
-class RecyclerAdapter(private var reps: List<Representative>)
+class RecyclerAdapter(var reps: List<Representative>)
     : RecyclerView.Adapter<RecyclerAdapter.RepresentativeHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepresentativeHolder {
@@ -25,10 +25,6 @@ class RecyclerAdapter(private var reps: List<Representative>)
     override fun onBindViewHolder(holder: RepresentativeHolder, position: Int) {
         val rep = reps[position]
         holder.bindRepresentative(rep)
-    }
-
-    fun setRepresentatives(representatives: List<Representative>) {
-        reps = representatives
     }
 
     class RepresentativeHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
@@ -47,25 +43,29 @@ class RecyclerAdapter(private var reps: List<Representative>)
             private val REP_KEY = "REP"
         }
 
+        /**
+         * Function that binds a representative to a view in the RecyclerView.
+         * @param representative the representative to be bound
+         */
         @SuppressLint("SetTextI18n")
         fun bindRepresentative(representative: Representative) {
             this.representative = representative
             val url: String = representative.photo_url.replace("http:", "https:", ignoreCase = true)
-            Glide.with(view)
+            Glide.with(view) // Load picture into view
                 .load(url)
-                .placeholder(R.drawable.unknown_person)
+                .placeholder(R.drawable.unknown_person) // Doubles as placeholder while loading, and default image if bad url or no image returned by API
                 .into(view.rep_portrait)
             view.rep_name.text = representative.name
             view.rep_office_riding.text = representative.elected_office +
                     (if (representative.elected_office.toLowerCase(Locale.US) == "mayor" || representative.elected_office.toLowerCase(Locale.US) == "chair") " of "
                     else " for ") + representative.district_name
-            view.rep_office_riding.text = view.rep_office_riding.text
-            if (!representative.party_name.isBlank()) {
-                view.rep_party.visibility = View.VISIBLE
+            view.rep_legislature.text = representative.representative_set_name
+            if (!representative.party_name.isBlank()) { // If party name exists and is not null
+                view.rep_party.visibility = View.VISIBLE // TextView for the party name appears
                 view.rep_party.text = representative.party_name
 
-                val contains: (String) -> Boolean = {party: String -> party in representative.party_name.toLowerCase(Locale.US)}
-                view.colour_bar.setBackgroundColor(Color.parseColor(
+                val contains: (String) -> Boolean = {party: String -> party in representative.party_name.toLowerCase(Locale.US)} // Lambda to check whether a matching substring is in the party name
+                view.colour_bar.setBackgroundColor(Color.parseColor( // Set the colour bar based on some of the most common parties' colours
                     when {
                         contains("green") -> "#3D9835"
                         contains("conservative") -> "#124072"
@@ -75,15 +75,14 @@ class RecyclerAdapter(private var reps: List<Representative>)
                         contains("bloc") -> "#5EB0DD"
                         contains("coalition avenir") -> "#00A8E7"
                         contains("parti québécois") -> "#15336F"
-                        else -> "#AFAFAF"
+                        else -> "#AFAFAF" // Gray if the party name doesn't match any on the list here
                     }
                 ))
             }
             else {
-                view.rep_party.visibility = View.GONE
-                view.colour_bar.setBackgroundColor(Color.parseColor("#AFAFAF"))
+                view.rep_party.visibility = View.GONE // Otherwise, hide the party TextView
+                view.colour_bar.setBackgroundColor(Color.parseColor("#AFAFAF")) // And set the colour bar to gray
             }
-            view.rep_legislature.text = representative.representative_set_name
         }
     }
 }
--- app/src/main/java/com/example/findmyrepresentatives/ResultsActivity.kt
+++ app/src/main/java/com/example/findmyrepresentatives/ResultsActivity.kt
@@ -12,7 +12,7 @@ import kotlinx.android.synthetic.main.activity_results.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.lang.IllegalArgumentException
+import kotlin.IllegalArgumentException
 
 /**
  * The results activity.
@@ -26,25 +26,28 @@ class ResultsActivity (): AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
 
-        linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager = LinearLayoutManager(this) // Responsible for positioning individual views
         recyclerView.layoutManager = linearLayoutManager
 
-        adapter = RecyclerAdapter(emptyList<Representative>())
+        adapter = RecyclerAdapter(emptyList()) // Responsible for making individual views for each representative
         recyclerView.adapter = adapter
 
-        val postalCode = intent.getStringExtra("postalCode")
+        val postalCode = intent.getStringExtra("postalCode") // Sent in from MainActivity
         if (postalCode != null) {
             getRepresentative(postalCode)
         }
 
-        back_button.setOnClickListener() {
-            Log.d("finishing", "up")
+        back_button.setOnClickListener() {// For if the postal code doesn't actually exist, user can press back button in two ways
             finish()
         }
     }
 
-    fun getRepresentative(postalCode: String) {
-
+    /**
+     * Function that calls the Represent API and updates the RecyclerView
+     * of the activity.
+     * @param postalCode the postal code that the user inputted
+     */
+    private fun getRepresentative(postalCode: String) {
         RepresentApi.retrofitService.getRepresentatives(postalCode).enqueue(
             object: Callback<RepresentDataSet> {
                 override fun onFailure(call: Call<RepresentDataSet>, t: Throwable) {
@@ -52,39 +55,39 @@ class ResultsActivity (): AppCompatActivity() {
                 }
 
                 override fun onResponse(call: Call<RepresentDataSet>, response: Response<RepresentDataSet>) {
-                    if (response.code() == 404) {
+                    if (response.code() == 404) { // Postal code is of a valid format, but doesn't actually exist
                         Log.d("invalid", postalCode)
-                        loading_list.visibility = View.GONE
-                        invalid_code.visibility = View.VISIBLE
-                        back_button.visibility = View.VISIBLE
+                        loading_list.visibility = View.GONE // Remove loading message
+                        invalid_code.visibility = View.VISIBLE // Display error message
+                        back_button.visibility = View.VISIBLE // Display additional button to go back
                     }
                     else if (response.body() == null) {
                         Log.d("null response", "shouldn't happen")
                     }
                     else {
-                        loading_list.visibility = View.GONE
-                        val body: RepresentDataSet = response.body()!!
-                        val reps: MutableList<Representative>
-                        if (body.representatives_centroid == null && body.representatives_concordance == null) {
+                        loading_list.visibility = View.GONE // Loading done
+                        val body: RepresentDataSet = response.body()!! // Body won't be null
+                        val reps: MutableList<Representative> // The API returns two lists of representatives based on two different methods (center and boundaries of postal code area)
+                        if (body.representatives_centroid == null && body.representatives_concordance == null) { // For some reason, no representatives found at this postal code (shouldn't happen)
                             throw NoSuchElementException()
                         }
-                        else if (body.representatives_centroid == null) {
-                            reps = body.representatives_concordance!!
+                        else if (body.representatives_centroid == null) { // Most representatives are normally in representatives_centroid
+                            reps = body.representatives_concordance!! // But if not, representatives_concordance is the entire list
                         }
                         else {
-                            reps = body.representatives_centroid
+                            reps = body.representatives_centroid // Otherwise, the list is representatives_centroid
                             reps.let {
-                                body.representatives_concordance?.let(it::addAll)
+                                body.representatives_concordance?.let(it::addAll) // And add anything from representatives_concordance as well (will still work if null)
                             }
                         }
-                        reps.apply {
-                            var ind : Int = indexOfFirst {
+                        reps.apply { // Sort the results (top -> federal representative -> provincial representative -> mayor -> city councillor -> anything else (school board trustees, etc.)
+                            var ind : Int = indexOfFirst { // Check if there is a city councillor
                                 it.elected_office == "Councillor"
                             }
-                            if (ind != -1) {
+                            if (ind != -1) { // If there is, move it to the start
                                 add(0, removeAt(ind))
                             }
-                            ind = indexOfFirst {
+                            ind = indexOfFirst { // Same for all the other high-priority representatives
                                 it.elected_office == "Mayor"
                             }
                             if (ind != -1) {
@@ -106,8 +109,8 @@ class ResultsActivity (): AppCompatActivity() {
                                 add(0, removeAt(ind))
                             }
                         }
-                        adapter.setRepresentatives(reps)
-                        adapter.notifyDataSetChanged()
+                        adapter.reps = reps // Update the RecyclerView data
+                        adapter.notifyDataSetChanged() // Trigger changing the RecyclerView
                     }
                 }
             }
